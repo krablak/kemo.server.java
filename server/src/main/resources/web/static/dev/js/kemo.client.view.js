c29624d7c61044a7f6fdc14b1ba3f598c6c4f233
krablak <michal.racek@twfrbr.com>
2016-06-21 18:45:23 +0200

    Added formatted function and check for empty user name.

--- server/src/main/resources/web/static/dev/js/kemo.client.view.js
+++ server/src/main/resources/web/static/dev/js/kemo.client.view.js
@@ -16,7 +16,10 @@ var kemo = function(kemo) {
 		})[0];
 	};
 
-	riot.tag2('kemo-chat', '\
+	riot
+			.tag2(
+					'kemo-chat',
+					'\
 			<div>\
 				<div class="row">\
 					<input type="text" kemo-type="username" placeholder="Place you username here..." value={username} onchange="{onUsernameChanged}" onblur="{onUsernameChanged}" class="four columns"/>\
@@ -35,130 +38,142 @@ var kemo = function(kemo) {
 						<button class="send-btn button-primary" kemo-type="send-btn" onclick="{onSendMessage}">Send</button>\
 					</div>\
 				</div>\
-			\</div>', '', '', function(opts) {
-		var self = this;
-		self.page = opts.page;
-		// Element id
-		self.id = self.root.getAttribute("id");
-		// Default encryption key
-		self.key = self.root.getAttribute("key");
-		// Max messages on screen
-		self.max_messages = 15;
-		// Messages queue
-		self.messages = new util.MiniQueue(self.max_messages, {
-			empty : true
-		});
-		// Send messages markers
-		self.sentMessages = new util.MiniQueue(self.max_messages);
+			\</div>',
+					'', '', function(opts) {
+						var self = this;
+						self.page = opts.page;
+						// Element id
+						self.id = self.root.getAttribute("id");
+						// Default encryption key
+						self.key = self.root.getAttribute("key");
+						// Max messages on screen
+						self.max_messages = 15;
+						// Messages queue
+						self.messages = new util.MiniQueue(self.max_messages, {
+							empty : true
+						});
+						// Send messages markers
+						self.sentMessages = new util.MiniQueue(self.max_messages);
 
-		// Handle on mount event to get info from rendered component
-		self.on('mount', function() {
-			// Get references to chat window elements
-			self.keyInp = self.root.querySelectorAll('input[kemo-type="key"]')[0];
-			self.usernameInp = self.root.querySelectorAll('input[kemo-type="username"]')[0];
-			self.messagesDiv = self.root.querySelectorAll('div[kemo-type="messages"]')[0];
-			self.messageInp = self.root.querySelectorAll('input[kemo-type="message"]')[0];
-			self.sendBtn = self.root.querySelectorAll('button[kemo-type="send-btn"]')[0];
-			// Try to connection when UI is ready
-			self.page.trigger('messaging-connect', newConnectEvent());
-		});
+						// Handle on mount event to get info from rendered
+						// component
+						self.on('mount', function() {
+							// Get references to chat window elements
+							self.keyInp = self.root.querySelectorAll('input[kemo-type="key"]')[0];
+							self.usernameInp = self.root.querySelectorAll('input[kemo-type="username"]')[0];
+							self.messagesDiv = self.root.querySelectorAll('div[kemo-type="messages"]')[0];
+							self.messageInp = self.root.querySelectorAll('input[kemo-type="message"]')[0];
+							self.sendBtn = self.root.querySelectorAll('button[kemo-type="send-btn"]')[0];
+							// Try to connection when UI is ready
+							self.page.trigger('messaging-connect', newConnectEvent());
+						});
 
-		// Creates new send event
-		var newSendEvent = function() {
-			return {
-				source : self,
-				key : self.keyInp.value,
-				message : "["+self.usernameInp.value+"] "+self.messageInp.value
-			};
-		}
+						// Formats message using user name and message
+						var formatMessage = function(userName, message) {
+							if (userName && userName.trim().length > 0) {
+								return "[" + userName + "] " + message
+							} else {
+								return message;
+							}
+						}
 
-		// Creates required new connection event
-		var newConnectEvent = function() {
-			return {
-				source : self,
-				key : self.keyInp.value
-			};
-		};
+						// Creates new send event
+						var newSendEvent = function() {
+							return {
+								source : self,
+								key : self.keyInp.value,
+								message : formatMessage(self.usernameInp.value, self.messageInp.value)
+							};
+						}
 
-		// Fills given content with given content
-		var fillWith = function(array, count, content) {
-			for (var i = 0; i < count; i++) {
-				array.add(content);
-			}
-		};
+						// Creates required new connection event
+						var newConnectEvent = function() {
+							return {
+								source : self,
+								key : self.keyInp.value
+							};
+						};
 
-		// Returns true when message was resolved as sent
-		var checkIsSent = function(message) {
-			var isSent = false;
-			if (message) {
-				var idx = self.sentMessages.items.indexOf(message);
-				if (idx > -1) {
-					isSent = true;
-					self.sentMessages.items.splice(idx, 1);
-				}
-			}
-			return isSent;
-		};
+						// Fills given content with given content
+						var fillWith = function(array, count, content) {
+							for (var i = 0; i < count; i++) {
+								array.add(content);
+							}
+						};
 
-		// Marks message as sent
-		var markSent = function(message) {
-			self.sentMessages.add(message);
-		};
+						// Returns true when message was resolved as sent
+						var checkIsSent = function(message) {
+							var isSent = false;
+							if (message) {
+								var idx = self.sentMessages.items.indexOf(message);
+								if (idx > -1) {
+									isSent = true;
+									self.sentMessages.items.splice(idx, 1);
+								}
+							}
+							return isSent;
+						};
 
-		// Performs message send operation
-		var doSendMessage = function() {
-			// Prepare and publish event
-			var sendEvent = newSendEvent();
-			self.page.trigger('messaging-send', sendEvent);
-			// Mark message content as sent
-			markSent(sendEvent.message);
-			// Clear message input value
-			self.messageInp.value = "";
-		};
+						// Marks message as sent
+						var markSent = function(message) {
+							self.sentMessages.add(message);
+						};
 
-		// Called when clicked on Send button
-		self.onSendMessage = function(e) {
-			doSendMessage();
-		}.bind(this);
+						// Performs message send operation
+						var doSendMessage = function() {
+							// Prepare and publish event
+							var sendEvent = newSendEvent();
+							self.page.trigger('messaging-send', sendEvent);
+							// Mark message content as sent
+							markSent(sendEvent.message);
+							// Clear message input value
+							self.messageInp.value = "";
+						};
 
-		// Called when pressed button within message input
-		self.onKeyChanged = function(e) {
-			self.page.trigger('ui-key-changed', self.keyInp.value);
-			return true;
-		}.bind(this);
+						// Called when clicked on Send button
+						self.onSendMessage = function(e) {
+							doSendMessage();
+						}.bind(this);
 
-		// Called when pressed button within message input
-		self.onMessageKeyPress = function(e) {
-			var key = e.which || e.keyCode;
-			if (key === 13) {
-				doSendMessage();
-			}
-			return true;
-		}.bind(this);
+						// Called when pressed button within message input
+						self.onKeyChanged = function(e) {
+							self.page.trigger('ui-key-changed', self.keyInp.value);
+							return true;
+						}.bind(this);
 
-		// Called on received message
-		self.on('ui-received', function(e) {
-			// Resolve if received message was sent by us
-			var isSentMessage = checkIsSent(e);
-			// Update data model
-			self.messages.add({
-				sent : isSentMessage,
-				message : e
-			});
-			
-			// Update component
-			self.update();
+						// Called when pressed button within message input
+						self.onMessageKeyPress = function(e) {
+							var key = e.which || e.keyCode;
+							if (key === 13) {
+								doSendMessage();
+							}
+							return true;
+						}.bind(this);
 
-			// Scroll to messages box bottom
-			self.messagesDiv.scrollTop = self.messagesDiv.scrollHeight;
-			
-			// Notify page that new received message was displayed
-			if(!isSentMessage){
-				self.page.trigger('ui-received-new',e);
-			}
-		});
+						// Called on received message
+						self.on('ui-received', function(e) {
+							// Resolve if received message was sent by us
+							var isSentMessage = checkIsSent(e);
+							// Update data model
+							self.messages.add({
+								sent : isSentMessage,
+								message : e
+							});
 
-	}, '{ }');
+							// Update component
+							self.update();
+
+							// Scroll to messages box bottom
+							self.messagesDiv.scrollTop = self.messagesDiv.scrollHeight;
+
+							// Notify page that new received message was
+							// displayed
+							if (!isSentMessage) {
+								self.page.trigger('ui-received-new', e);
+							}
+						});
+
+					}, '{ }');
 
 	riot.tag('chat-line', '\
 			<div if={!empty} class="{sent: sent}">{message}</div>\
